ea1f5a8aaec892ab93f686ddc0cfae8c4c93a0aa
Panagiotis Iatrou <iatrou.panag@gmail.com>
2018-01-24 21:33:36 +0200

    Added project

---
+++ HomingMissles2D/Assets/Resources/Scripts/HomingMissile.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class HomingMissile : MonoBehaviour
+{
+
+	private GameObject TargetGO;
+	private float turnSpeed = 3f;
+	private float speed = 3f;
+
+	private bool noTargets = false;
+
+	private void Start()
+	{
+		updateTarget();
+	}
+
+	// Locks the nearest target
+	private void updateTarget()
+	{
+		Target[] targets = FindObjectsOfType<Target>();
+		if (targets.Length == 0)
+		{
+			noTargets = true;
+			return;
+		}
+		for (int i = 0; i < targets.Length; i++)
+		{
+			if (TargetGO == null)
+			{
+				TargetGO = targets[i].gameObject;
+				continue;
+			}
+
+			if (Vector3.Distance(transform.position, targets[i].transform.position) < Vector3.Distance(transform.position, TargetGO.transform.position))
+				TargetGO = targets[i].gameObject;
+
+		}
+	}
+
+	private void Update()
+	{
+		if (noTargets)
+			return;
+
+		// Move and rotate missile
+		transform.position += transform.right * speed * Time.deltaTime;
+		Vector3 direction = (TargetGO.transform.position - transform.position).normalized;
+		Vector3 dir = TargetGO.transform.position - transform.position; float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+		Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+		transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * turnSpeed);
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Homing-Missiles-2D
 Small project with homing missiles that automatically follow the nearest target
+
+# How to use:
+
+1) Add the HomingMissile.cs script to every object you want to act as a homing missile.
+2) Add the Target.cs script to every object you want the missiles to follow.
+3) Done!
+
+# Note:
+When a missile object is created it automatically finds the nearest Target using the updateTarget() method located at HomingMissile.cs. You will have to call it manually to find the nearest Target again (Maybe call it a few times in Update())
